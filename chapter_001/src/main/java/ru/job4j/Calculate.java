2423b77e2eeb9a5272781c6649460adb01e1e791
Konstantin Gnedash <kgnedash@gmail.com>
2017-10-30 15:18:09 +0300

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
-package ru.job4j;
+﻿package ru.job4j;
 
+/**
+ *Class Calculate
+ *author kgnedash
+ *@since 27.10.2017
+ *@version 1
+*/
 public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
