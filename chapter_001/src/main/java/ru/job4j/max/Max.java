fe3dc911b5e47a77a823ac37752a1c9a973e32f4
Konstantin Gnedash <kgnedash@gmail.com>
2017-11-10 18:13:14 +0300

    3.1 Максимум из двух чисел[#189]
    3.1. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.max;
+
+/**
+ *Class Max
+ *author kgnedash
+ *@since 10.11.2017
+ *@version 1
+ */
+ 
+ public class Max {
+	  public int max(int first, int second) {
+		  return (first > second) ? first : second;
+	  }
+ }
