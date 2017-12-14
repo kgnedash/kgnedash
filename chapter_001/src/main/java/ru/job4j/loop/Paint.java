bb59fd458e5e8311dc564836b8bfa419e4db7904
Konstantin Gnedash <kgnedash@gmail.com>
2017-12-14 13:08:07 +0300

    4.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,61 +1,84 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
- * Class Paint painting pyramid in pseudo-graphic.
+ * Class Paint paints pyramid in pseudo-graphic.
  *
  * @author kgnedash
  * @version 1
- * @since 13.12.17
+ * @since 14.12.17
  */
 public class Paint {
+
+    /**
+     * The method constructs the right half of the pyramid
+     *
+     * @param h height
+     * @return result
+     */
     public String rightTrl(int h) {
-        int w = h;
-        StringBuilder screen = new StringBuilder();
-        String ln = System.lineSeparator();
-        for (int i = 0; i != h; i++) {
-            for (int j = 0; j != w; j++) {
-                if (i >= j) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(ln);
-        }
-        return screen.toString();
+        return this.loopBy(
+                h,
+                h,
+                (i, j) -> i >= j
+        );
     }
 
+    /**
+     * The method constructs the left half of the pyramid
+     *
+     * @param h height of the pyramid
+     * @return result
+     */
     public String leftTrl(int h) {
-        int w = h;
-        StringBuilder screen = new StringBuilder();
-        String ln = System.lineSeparator();
-        for (int i = 0; i != h; i++) {
-            for (int j = 0; j != w; j++) {
-                if (i >= w - j - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(ln);
-        }
-        return screen.toString();
+        return this.loopBy(
+                h,
+                h,
+                (i, j) -> i >= h - j - 1
+        );
     }
 
-    public String piramid(int h) {
-        int w = (h * 2) - 1;
+    /**
+     * The method constructs the pyramid
+     *
+     * @param h height
+     * @return result
+     */
+    public String pyramid(int h) {
+        return this.loopBy(
+                h,
+                2 * h - 1,
+                (i, j) -> i >= h - j - 1 && i + h - 1 >= j
+        );
+    }
+
+    /**
+     * New universal method for conditions
+     *
+     * @param h       height of the pyramid
+     * @param w       weight of the pyramid
+     * @param predict java Generics
+     * @return
+     */
+    private String loopBy(int h, int w, BiPredicate<Integer, Integer> predict) {
+        //creating an object of class StringBuilder
         StringBuilder screen = new StringBuilder();
-        String ln = System.lineSeparator();
+        //Loop for building the pyramid, i counts rows, j counts columns
         for (int i = 0; i != h; i++) {
             for (int j = 0; j != w; j++) {
-                if (i >= h - j - 1 && i + h - 1 >= j) {
+                if (predict.test(i, j)) {
+                    //adding the symbol "^" in the screen object
                     screen.append("^");
                 } else {
+                    //adding the symbol " " in the screen object
                     screen.append(" ");
                 }
             }
-            screen.append(ln);
+            //adding the line break
+            screen.append(System.lineSeparator());
         }
+        //returns the result
         return screen.toString();
     }
 }
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -2,7 +2,9 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
+
 import java.util.StringJoiner;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -11,14 +13,21 @@ import static org.junit.Assert.*;
  *
  * @author kgnedash
  * @version 1
- * @since 13.12.17
+ * @since 14.12.17
  */
 public class PaintTest {
+    /**
+     * Method for testing the right half of the pyramid
+     */
     @Test
     public void whenPyramid4Right() {
+        //creates an object of class Paint
         Paint paint = new Paint();
+        //calculates the result of the rightTrl method
         String rst = paint.rightTrl(4);
+        //prints out the result
         System.out.println(rst);
+        //comparing the result with expected
         assertThat(rst,
                 is(
                         new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
@@ -31,11 +40,18 @@ public class PaintTest {
         );
     }
 
+    /**
+     * Method for testing the left half of the pyramid
+     */
     @Test
     public void whenPyramid4Left() {
+        //creates an object of class Paint
         Paint paint = new Paint();
+        //calculates the result of the leftTrl method
         String rst = paint.leftTrl(4);
+        //prints out the result
         System.out.println(rst);
+        //comparing the result with expected
         assertThat(rst,
                 is(
                         new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
@@ -48,21 +64,35 @@ public class PaintTest {
         );
     }
 
+    /**
+     * Method for testing the pyramid with height equals two
+     */
     @Test
-    public void whenPiramidWithHeightTwoThenStringWithTwoRows() {
+    public void whenPyramidWithHeightTwoThenStringWithTwoRows() {
+        //creates an object of class Paint
         Paint paint = new Paint();
-        String result = paint.piramid(2);
+        //calculates the result of the pyramid method with height equals 2
+        String result = paint.pyramid(2);
+        //creates the expected result
         final String line = System.getProperty("line.separator");
         String expected = String.format(" ^ %s^^^%s", line, line);
+        //comparing the result with expected
         assertThat(result, is(expected));
     }
 
+    /**
+     * Method for testing the pyramid with height equals three
+     */
     @Test
-    public void whenPiramidWithHeightThreeThenStringWithThreeRows() {
+    public void whenPyramidWithHeightThreeThenStringWithThreeRows() {
+        //creates an object of class Paint
         Paint paint = new Paint();
-        String result = paint.piramid(3);
+        //calculates the result of the pyramid method with height equals 3
+        String result = paint.pyramid(3);
+        //creates the expected result
         final String line = System.getProperty("line.separator");
         String expected = String.format("  ^  %s ^^^ %s^^^^^%s", line, line, line);
+        //comparing the result with expected
         assertThat(result, is(expected));
     }
 }
